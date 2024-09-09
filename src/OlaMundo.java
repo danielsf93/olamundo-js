24b24f56c33dd84e436a5efab3344e137b383c31
Daniel Silva Ferreira <danielsilva@usp.br>
2024-09-09 09:42:37 -0300

    Add files via upload

---
+++ src/OlaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author acolhe
+ */
+public class OlaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String meuPrimeiroPrograma;
+        meuPrimeiroPrograma = "Hello World";
+        System.out.println(meuPrimeiroPrograma);
+    }
+
+}
